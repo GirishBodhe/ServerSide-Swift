3fe409eb35ab0a22c4d282130b2f107d22c41d19
GirishBodhe <bodhegirish@gmail.com>
2018-05-11 15:54:30 +0530

    log done 2nd
    done

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -3,7 +3,7 @@ import Vapor
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // your code here
-    drop.get("/") { request in
+    drop.get() { request in
         return "Hello, Manoj...!"
     }
     drop.get("Girish") { request in
