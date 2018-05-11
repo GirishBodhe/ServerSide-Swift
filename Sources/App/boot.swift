e0dd624fcf2dfa183c60722b4d4971cab48d1972
GirishBodhe <bodhegirish@gmail.com>
2018-05-11 16:00:59 +0530

    v1-test
    Done

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -3,10 +3,24 @@ import Vapor
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
     // your code here
-    drop.get() { request in
-        return "Hello, Manoj...!"
+   
+    
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, world "
     }
-    drop.get("Girish") { request in
+    router.get("hello","Girish") { req in
+        return "Hello, world \n \t - Girish!"
+    }
+    router.get("Girish") { request in
         return "Hello, Girish...!"
     }
+    router.get() { req in
+        return "Hello, world....!"
+    }
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
