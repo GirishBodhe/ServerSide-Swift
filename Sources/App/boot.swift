cd642b78d4a01c101ad4c0d8eabcf69adf99e25a
GirishBodhe <bodhegirish@gmail.com>
2018-05-11 16:15:07 +0530

    v1_test
    Done

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -4,23 +4,4 @@ import Vapor
 public func boot(_ app: Application) throws {
     // your code here
    
-    
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world "
-    }
-    router.get("hello","Girish") { req in
-        return "Hello, world \n \t - Girish!"
-    }
-    router.get("Girish") { request in
-        return "Hello, Girish...!"
-    }
-    router.get() { req in
-        return "Hello, world....!"
-    }
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,10 +3,20 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+    // Basic "Hello, world!" example
+    router.get("Hello") { req in
+        return "Hello, world "
     }
-
+    router.get("hello","Girish") { req in
+        return "Hello, world \n \t - Girish!"
+    }
+    router.get("Girish") { request in
+        return "Hello, Girish...!"
+    }
+    router.get() { req in
+        return "Hello, world....!"
+    }
+   
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
