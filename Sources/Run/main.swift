654309b232c98689c84c012683e0fb239d1fe178
GirishBodhe <bodhegirish@gmail.com>
2018-05-14 13:30:37 +0530

    Main.swift
    Resolve the port that we want the server to listen on.

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,3 +1,16 @@
-import App
+//import App
+//
+//try app(.detect()).run()
 
-try app(.detect()).run()
+
+// in main.swift...
+import Foundation
+
+// Resolve the port that we want the server to listen on
+let port: Int
+let defaultPort = 80
+if let requestedPort = ProcessInfo.processInfo.environment["PORT"] {
+    port = Int(requestedPort) ?? defaultPort
+} else {
+    port = defaultPort
+}
